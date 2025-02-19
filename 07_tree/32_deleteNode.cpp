79e6bf063eae3b21a3e32546e0c1393196e6fe2b
zheng <Fu_zheng@outlook.com>
2025-02-19 12:12:51 +0800

    2025/02/19

--- 07_tree/32_deleteNode.cpp
+++ 07_tree/32_deleteNode.cpp
@@ -10,11 +10,14 @@
 using namespace std;
 
 // 题目描述
-// https://leetcode.cn/problems/insert-into-a-binary-search-tree/
+// https://leetcode.cn/problems/delete-node-in-a-bst/description/
 
-// 给定二叉搜索树（BST）的根节点 root 和要插入树中的值 value ，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+// 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
 
-// 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+// 一般来说，删除节点可分为两个步骤：
+
+// 首先找到需要删除的节点；
+// 如果找到了，删除它。
 
 // Definition for a binary tree node.
 struct TreeNode {
@@ -27,9 +30,54 @@ struct TreeNode {
 };
 
 class Solution {
+private:
+    void traverse(TreeNode* cur, int key, TreeNode*& pre){
+        if(cur == NULL) return;
+
+        if(cur->val == key){
+            // 目标节点是左子树节点
+            if(pre->val > key){
+                // 右子树的最小值作为被删除点
+                if(cur->right != nullptr){
+
+                    pre->left = ;
+
+                }
+                else{
+                    pre->left = cur->left;
+                }
+            }
+            // 目标节点是右子树节点
+            else if(pre->val < key){
+                if(cur->left != nullptr){
+                    pre->right = cur->left;
+                }
+                else{
+                    pre->right = cur->right;
+                }
+            }
+            delete cur;
+            return;
+        }
+
+        pre = cur;
+        if(key < cur->val) traverse(cur->left, key, pre);
+        if(key > cur->val) traverse(cur->right, key, pre);
+
+    }
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        
+        if(root == NULL) return nullptr;
+        // 删除根节点
+        if(root->val == key){
+            if(root->left != nullptr) return root->left;
+            if(root->right != nullptr) return root->right;
+            return nullptr;
+        }
+        // 不删除根节点
+        TreeNode* pre = nullptr;
+        traverse(root, key, pre);
+        return root;
     }
 };
 
