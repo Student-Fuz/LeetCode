5ea11b49d5c269e4f349d9e9bd4ef1627b3c9250
zheng <Fu_zheng@outlook.com>
2025-03-05 15:12:53 +0800

    2025/03/05

--- 99_practice/Hot100/15_dp/15_09_canPartition.cpp
+++ 99_practice/Hot100/15_dp/15_09_canPartition.cpp
@@ -13,17 +13,47 @@ using namespace std;
 
 // Solution
 class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
-        int sumVal = 0;
-
-        for(int i = 0; i < nums.size(); i++){
-            sumVal += nums[i];
+    public:
+        bool canPartition(vector<int>& nums) {
+            if(nums.size() == 0)
+                return false;
+            int sumVal = 0;
+    
+            for(int i = 0; i < nums.size(); i++){
+                sumVal += nums[i];
+            }
+    
+            int target = sumVal/2;
+    
+            // 奇数
+            if(sumVal - target != target)
+                return false;
+    
+            vector<vector<bool>> dp(nums.size(), vector<bool>(target+1, false));
+            dp[0][0] = true;
+            if(nums[0] < target)
+                dp[0][nums[0]] = true;
+            if(nums[0] == target)
+                return true;
+    
+            for(int i = 1; i < nums.size(); i++){
+                dp[i][0] = true;
+                for(int j = 0; j <= target; j++){
+                    if(dp[i-1][j] == true){
+                        dp[i][j] = true;
+                        int sum = j+nums[i];
+                        if(sum <= target){
+                            dp[i][sum] = true;
+                            if(sum == target)
+                                return true;
+                        }
+                    }
+                }
+            }
+            
+            return false;
         }
-
-        
-    }
-};
+    };
 
 int main(){
 
