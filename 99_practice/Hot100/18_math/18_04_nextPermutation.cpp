288631f7d036a516db824f3b29f17ca064d57b77
zheng <Fu_zheng@outlook.com>
2025-03-14 11:31:27 +0800

    2025/03/14

--- 99_practice/Hot100/18_math/18_04_nextPermutation.cpp
+++ 99_practice/Hot100/18_math/18_04_nextPermutation.cpp
@@ -16,6 +16,9 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
+        for(int i = nums.size()-2; i >= 0; i--){
+            if(nums[i] > nums[i+1])
+        }
     }
 };
 
