a063254f4cbe1ac4d20351db58f421cccfd2f67a
zheng <Fu_zheng@outlook.com>
2025-03-31 22:00:37 +0800

    2025/03/31

--- 99_practice/Hot100/13_stack/13_03_decodeString.cpp
+++ 99_practice/Hot100/13_stack/13_03_decodeString.cpp
@@ -17,14 +17,26 @@ class Solution {
 public:
     string decodeString(string s) {
         string result;
-        stack<char> char_stack;
+        string result_1;
+        vector<char> char_stack;
 
         for(const char char_s : s){
             if(char_s == ']'){
-                result.push_back(char)
+                while(char_stack.back() != '['){
+                    result.push_back(char_stack.back());
+                    char_stack.pop_back();
+                }
+                char_stack.pop_back();
+                for(int i = 0; i < char_stack.back(); i++){
+                    result_1 += result;
+                }
+                
             }
 
         }
+
+        return result_1;
+
     }
 };
 
